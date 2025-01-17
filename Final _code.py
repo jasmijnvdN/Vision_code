f48db392871f2b1220557c5072e3125eee0d0a1d
jasmijnvdN <156360388+jasmijnvdN@users.noreply.github.com>
2025-01-17 16:25:39 +0100

    Update Final _code.py

--- Final _code.py
+++ Final _code.py
@@ -52,9 +52,9 @@ while True:
         if key == ord('s'):
             cv2.imwrite("webcam_img.jpg", webcam_img)
             
-            img_gray = cv2.cvtColor(webcam_img, cv2.COLOR_BGR2GRAY)   #grey image
+            gray_image = cv2.cvtColor(webcam_img, cv2.COLOR_BGR2GRAY)   #grey image
             blurred_img = cv2.GaussianBlur(webcam_img, (21, 21), 30)  #used to reduce noise in image
-            edges = cv2.Canny(img_gray, 40, 140)                      #edge detection
+            edges = cv2.Canny(gray_image, 40, 140)                      #edge detection
             contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)  #finds contours
             
             mask = np.zeros_like(edges)
@@ -76,21 +76,20 @@ while True:
                         max_area = area  # Update the maximum area
                         
                         M = cv2.moments(cnt)
-                        if M['m00'] != 0:  #zodat er niet gedeeld word door 0
-                            midden = (int(M['m10'] / M['m00']), int(M['m01'] / M['m00'])) #m10 voor x m01 voor y
-                            #geeft coordinaten voor het midden
+                        if M['m00'] != 0:  #to not / by 0
+                            midden = (int(M['m10'] / M['m00']), int(M['m01'] / M['m00'])) #m10 for x m01 for y
+                            #midden= coodinates for the center of the shape (x,y)
 
                         rightmost_pixel = tuple(cnt[cnt[:, :, 0].argmax()][0])  # Rightmost point of the contour
 
-                        #midden van bloje bepalen
+    
                         x, y, w, h = cv2.boundingRect(cnt)
                         center = (x + w // 2, y + h // 2)
                         shape_centers.append(center)
-                        
             # Calculate the line between the centroid and the rightmost pixel
             dx = abs(rightmost_pixel[0] - center_of_shape[0])  
             dy = abs(rightmost_pixel[1] - center_of_shape[1])  
-            line_length = int(np.sqrt(dx ** 2 + dy ** 2))  #pythagoras
+            line_length = int(np.sqrt(dx ** 2 + dy ** 2))  #pythagoras 
             
             
             # print("Line Length:", line_length ,"pixels") test
@@ -103,16 +102,16 @@ while True:
             meest_rechter_pixel_x=(rightmost_pixel[0])
             meest_rechter_pixel_y=(rightmost_pixel[1])
             
-            vaste_coordinaat_van_PLC_x= -4.47
+            vaste_coordinaat_van_PLC_x= -4.47#needs to be changed
             vaste_coordinaat_van_PLC_y= -40.0
             
-            x_coordinaat_voor_meest_rechter_pixel= ((meest_rechter_pixel_x/4.0)*-1)+ vaste_coordinaat_van_PLC_x#hoeveelheid mm + coordinaat robot
+            x_coordinaat_voor_meest_rechter_pixel= ((meest_rechter_pixel_x/4.0)*-1)+ vaste_coordinaat_van_PLC_x#amount of  mm + coordinate robot
             y_coordinaat_voor_meest_rechter_pixel= (meest_rechter_pixel_y/4.0)+ vaste_coordinaat_van_PLC_y
             
             x_coordinaat_van_midden= x_coordinaat_voor_meest_rechter_pixel + dx_length_in_mm
             y_coordinaat_van_midden=y_coordinaat_voor_meest_rechter_pixel + dy_length_in_mm
             
-            write_lreal_db(22, 16, x_coordinaat_van_midden )#verstuuren pixel (misschien x en y nog omdraaien)
+            write_lreal_db(22, 16, x_coordinaat_van_midden )#send pixel to PLC(misschien x en y nog omdraaien)
             write_lreal_db(22, 24, y_coordinaat_van_midden )
             print("x_coordinaat_van_midden", x_coordinaat_van_midden)
             print("y_coordinaat_van_midden", y_coordinaat_van_midden)
@@ -132,34 +131,34 @@ while True:
 
                 number_of_vertices = len(approx)
                 shape_name = ""
-                color = (0, 0, 0) #zwart.
+                color = (0, 0, 0) #black.
 
-                area = cv2.contourArea(cnt)# area uitgedrukt in pixels
-                if area < 200: #ruis in achterdrond vermijden en kleine vormen negeren 
+                area = cv2.contourArea(cnt)# area uin pixels!
+                if area < 200: #ignore shapes detected under 200 pixels
                     continue
 #
             shape_name = "Square"
-            if 92000 <= area <= 92500: #hoeveel heid pixels die gedetecteerd kunnen worden
+            if 92000 <= area <= 92500: #tests show that the area detected falls between these numbers
                 color = (0,255,0)  
                 print("square detected")
-                write_bool(22, 2, 0, 1) #offset nog aanpassen
+                write_bool(22, 2, 0, 1) #change offset 
                 
         elif num_vertices == 3:  # Triangle
             shape_name = "Triangle"
-            if 180000 <= area <= 190000:
-                color = (0,255,0) 
+            if 180000 <= area <= 190000: #tests show that the area detected falls between these numbers
+                colo = (0,255,0) 
                 print("orange or green triangle")
-                write_bool(22, 2, 0, 1) #
+                write_bool(22, 2, 0, 1) #change offset 
                 
-            elif 45000 <= area <= 47000:
+            elif 45000 <= area <= 47000: #tests show that the area detected falls between these numbers
                 color = (0,255,0) 
                 print("white or dark blue triangle")
-                write_bool(22, 2, 0, 1) #
+                write_bool(22, 2, 0, 1) #change offset 
                 
-            elif 91000 <= area <= 93000:
+            elif 91000 <= area <= 93000: #tests show that the area detected falls between these numbers
                 color = (0,255,0) 
                 print("red triangle")
-                write_bool(22, 2, 0, 1)#
+                write_bool(22, 2, 0, 1)  #change offset 
             else:
                 continue 
 
@@ -169,18 +168,18 @@ while True:
             print("Maximale area:", max_area:.2f," pixels")
 
             cv2.line(show_image, midden, rightmost_pixel, (0, 255, 0, 2)) 
-            cv2.line(show_image, midden, (rightmost_pixel[0], centroid[1]), (0, 255,0) )#lijn van y 
-            cv2.line(show_image, midden, (centroid[0], rightmost_pixel[1]), (0,255,0), 2) #lijn van x
+            cv2.line(show_image, midden, (rightmost_pixel[0], centroid[1]), (0, 255,0) )#y line
+            cv2.line(show_image, midden, (centroid[0], rightmost_pixel[1]), (0,255,0), 2) #x line
             
             for center in shape_centers:
-                cv2.circle(show_image, center, 5, (0, 255, 0), -1)  #groene punt in het midden aantonen 5=radius van circkel, 
+                cv2.circle(show_image, center, 5, (0, 255, 0), -1)  #green dot in center of shape, 5=radius of circkel, 
 
-            cv2.imshow("Detected Shapes with Lines", show_image)# foto met lijnen
+            cv2.imshow("Detected Shapes with Lines", show_image)# foto with lines
 
             plt.subplot(121), plt.imshow(cv2.cvtColor(final_imgage, cv2.COLOR_BGR2RGB))
-            plt.title('Geblurde achtergrond'), plt.xticks([]), plt.yticks([])#laat foto met geblurede achtergrond zien
+            plt.title('Geblurde achtergrond'), plt.xticks([]), plt.yticks([])#fot with blurred backround
             plt.subplot(122), plt.imshow(cv2.cvtColor(show_image, cv2.COLOR_BGR2RGB))
-            plt.title('Gevonden shapes met lijnen'), plt.xticks([]), plt.yticks([])#laat gevonden shapes zien
+            plt.title('Gevonden shapes met lijnen'), plt.xticks([]), plt.yticks([])#foto with detected shapes drawn on image
 
             plt.show()
 
